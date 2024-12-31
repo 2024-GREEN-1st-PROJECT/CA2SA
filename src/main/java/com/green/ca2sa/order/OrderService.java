2c1c15e03f2f17714d13b91795752327bfbf116a
leejunyoung228 <80664840+leejunyoung228@users.noreply.github.com>
2024-12-31 10:28:51 +0900

    2024-12-31 10:28

--- src/main/java/com/green/ca2sa/order/OrderService.java
+++ src/main/java/com/green/ca2sa/order/OrderService.java
@@ -14,7 +14,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.*;
-import java.util.concurrent.atomic.AtomicLong;
 
 @Service
 @Slf4j
@@ -28,44 +27,44 @@ public class OrderService {
     public int PostOrder(OrderPostReq p) {
         long startTime = System.currentTimeMillis();
         log.info("OrderPostReq:{}", p);
-        int result = orderMapper.insOrder(p);
-        // xml 에 foreach 문 사용해서 올리기
-        List<OrderMenuPostReq> menuList = p.getMenuList().stream()
-                .peek(item -> item.setOrderId(p.getOrderId())) //오더 아이디 다 집어 넣어주는거 그냥 for문 돌리면서 값 넣어주는거로 보면 된다
-                .toList();
+        int insOrderRes = orderMapper.insOrder(p);
+        if (insOrderRes == 0) {
+            log.error("insert order fail");
+        }
 
-        int result2 = orderMenuMapper.insOrderMenu(menuList);
-        //db에 넣는용도로 만든 변수. 단순 데이터 저장
+        List<OrderMenuPostReq> menuList = p.getMenuList();
+        menuList.forEach(item -> item.setOrderId(p.getOrderId()));
 
-        AtomicLong orderMenuId = new AtomicLong(menuList.get(0).getOrderMenuId());
-        List<OrderMenuOptionPostReq> optionList = menuList.stream()
-                .flatMap(item -> {
-                    long currentOrderMenuId = orderMenuId.getAndIncrement(); // 현재 메뉴에 고유 ID 설정
-                    item.getOptions().forEach(option -> option.setOrderMenuId(currentOrderMenuId)); // 옵션에 ID 설정
-                    return item.getOptions().stream(); // 옵션 스트림 반환
-                })
-                .toList();
-//        List<OrderMenuOptionPostReq> optionList = new LinkedList<>();
-//        long orderMenuId = menuList.get(0).getOrderMenuId();
-//        for (OrderMenuPostReq item : p.getMenuList()) {
-//            for (OrderMenuOptionPostReq option : item.getOptions()) { // 옵션
-//                option.setOrderMenuId(orderMenuId); // 오더 메뉴아이디 받거 세팅하고
-//                optionList.add(option); // 옵션을 추가한다
-//            }
-//            orderMenuId++;   //그리고 메뉴 추가
-//        }
-        int result3 = orderMenuOptionMapper.insOrderMenuOption(optionList);
-        //db에 넣는용도로 만든 변수. 단순 데이터 저장
+        int insOrderMenuRes = orderMenuMapper.insOrderMenu(menuList);
+        if (insOrderMenuRes == 0) {
+            log.error("insert order menu fail");
+        }
+
+        List<OrderMenuOptionPostReq> optionList = new ArrayList<>();
+        long orderMenuId = menuList.get(0).getOrderMenuId();
+        for (OrderMenuPostReq item : menuList) {
+            for (OrderMenuOptionPostReq option : item.getOptions()) {
+                option.setOrderMenuId(orderMenuId);
+                optionList.add(option);
+            }
+            orderMenuId++;
+        }
+
+        int insOrderMenuOptionRes = orderMenuOptionMapper.insOrderMenuOption(optionList);
+        if (insOrderMenuOptionRes == 0) {
+            log.error("insert order menu option fail");
+        }
+
         log.info("post order cost time:{}", System.currentTimeMillis() - startTime);
-        return result;
+
+        return insOrderRes;
     }
 
     public List<OrderGetRes> GetOrderList(OrderGetReq p) {
-
         return orderMapper.getOrderList(p);
     }
 
-    public int cancelOrder(OrderCancelReq p){
+    public int cancelOrder(OrderCancelReq p) {
         return orderMapper.cancelOrder(p);
 
     }
