b520ee06dc9db1c516339f64e730e48dbf10efd9
leejunyoung228 <80664840+leejunyoung228@users.noreply.github.com>
2025-01-02 09:30:37 +0900

    2025-01-02 09:30

--- src/main/java/com/green/ca2sa/cafe/model/CafeGetReq.java
+++ src/main/java/com/green/ca2sa/cafe/model/CafeGetReq.java
@@ -5,7 +5,6 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.beans.ConstructorProperties;
-import java.math.BigDecimal;
 
 @Getter
 @Setter
@@ -17,11 +16,11 @@ public class CafeGetReq {
     @Schema(name = "max_distance", defaultValue = "1000",description = "미기입시 1000m")
     private long maxDistance;
     @Schema(name = "user_latitude")
-    private BigDecimal userLatitude;
+    private double userLatitude;
     @Schema(name = "user_longitude")
-    private BigDecimal userLongitude;
+    private double userLongitude;
     @ConstructorProperties({"search_menu_name", "search_cafe_name","max_distance", "user_latitude", "user_longitude"})
-    public CafeGetReq(String searchMenuName, String searchCafeName, Long maxDistance, BigDecimal userLatitude, BigDecimal userLongitude) {
+    public CafeGetReq(String searchMenuName, String searchCafeName, Integer maxDistance, double userLatitude, double userLongitude) {
         this.searchMenuName = searchMenuName;
         this.searchCafeName = searchCafeName;
         this.maxDistance = (maxDistance == null ? 1000 : maxDistance);
